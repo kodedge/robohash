60097f25419d39a9db847ff6afca5befcd2e8791
Swapneel Gosavi <kodedge.technologies@gmail.com>
2022-09-30 11:03:27 +0530

    Updates

---
+++ precache-manifest.062871a1812eb2b90d121bfabc9bb737.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2efc138c15702f6c25b2",
+    "url": "/robohash/static/css/main.c3487f54.chunk.css"
+  },
+  {
+    "revision": "2efc138c15702f6c25b2",
+    "url": "/robohash/static/js/main.2efc138c.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robohash/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "4a9df8f947a567d0321b",
+    "url": "/robohash/static/css/2.b2b6d146.chunk.css"
+  },
+  {
+    "revision": "4a9df8f947a567d0321b",
+    "url": "/robohash/static/js/2.4a9df8f9.chunk.js"
+  },
+  {
+    "revision": "d67f98fe5bb2ba592265",
+    "url": "/robohash/static/js/runtime~main.d67f98fe.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robohash/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "312258e9b3de4dd4f553d8f630d646d6",
+    "url": "/robohash/index.html"
+  }
+];
